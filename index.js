a48b960f356374ed55df499a50493d847d57fe73
beso <beskapoladishvili@gmail.com>
2022-04-24 20:45:29 +0400

    added comments

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const fast = document.getElementById('fast')
 
 toggles.forEach(toggle => {
   toggle.addEventListener('change', (e) => {
+    //if three of them are checked then the last target will uncheck appropriate checkbox 
     if (good.checked && cheap.checked && fast.checked) {
       if (good == e.target) {
         cheap.checked = false
